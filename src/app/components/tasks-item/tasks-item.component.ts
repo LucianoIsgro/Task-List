31da4caa2c73596fb65e6d536999690bf7d459fe
LucianoIsgro <lucianisgro@hotmail.com>
2023-03-30 23:34:25 -0300

    lista de items

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,14 @@
+import { Component,Input } from '@angular/core';
+import { Task } from '../Task';
+import { TASKS } from '../mock-task';
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent {
+
+@Input() task:Task=TASKS[0];
+
+
+}
