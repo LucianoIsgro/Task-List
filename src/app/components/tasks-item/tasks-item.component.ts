15e79cea9205d4269e79a7caf2aa3063ec8322d3
LucianoIsgro <lucianisgro@hotmail.com>
2023-04-11 15:20:16 -0300

    Task-list terminada, con el eliminar,agregar  y otros agregados

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component,Input } from '@angular/core';
+import { Component,Input,Output,EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { TASKS } from '../../mock-task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons'; //esto nos va a dar el icono para sacar tareas, es una cruz
@@ -11,6 +11,22 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons'; //esto nos va a dar
 export class TasksItemComponent {
 
 @Input() task:Task=TASKS[0];
+@Output() onDeleteTask:EventEmitter<Task> = new EventEmitter();
+@Output() onToggleReminder:EventEmitter<Task>=new EventEmitter(); 
 faTimes=faTimes; // el icono, despues le vamos a agregar las funcionalidades
+
+constructor(){}
+ngOnInit():void{}
+
+onDelete(task:Task){
+this.onDeleteTask.emit(task);
+
+}
+
+onToggle(task:Task){
+
+  this.onToggleReminder.emit(task);
+
+}
 
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -15,10 +15,32 @@ constructor(private taskService: TaskService) {
 
 ngOnInit(): void{
   //Like promise
-   this.taskService.getTasks().subscribe((tasks)=>[this.tasks=tasks]
+   this.taskService.getTasks().subscribe((tasks)=>(this.tasks=tasks)
    );
-
+   
 }
-  
+deleteTask(task:Task){
+  this.taskService.deleteTask(task)
+  .subscribe(
+    ()=>(
+      this.tasks=this.tasks.filter( (t) =>{
+      return t.id !== task.id
+    })
+    ))
+}
+
+toggleReminder(task:Task){
+  task.reminder=!task.reminder //solo es un cambio desde el lado del front, solo visual. No cambia nada en la base de datos, lo cual yo creo que deberia pasar pero todavia no llegamos ahi
+  this.taskService.updateTaskReminder(task).subscribe();
+}
+
+addTask(task:Task){
+  this.taskService.addTask(task).subscribe((task)=>(
+    this.tasks.push(task)
+
+    )
+  ) 
+}
+
 
 }
